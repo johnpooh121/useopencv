0727680b3dc01fefc2b0a2c6d7edb50a2f2767a5
impooh121 <impooh121@kaist.ac.kr>
2022-01-14 13:15:39 +0900

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,19 @@
+#include <jni.h>
+#include <string>
+#include "opencv2/opencv.hpp"
+#include "vo_features.h"
+
+using namespace cv;
+
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_useopencv_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject thiz,
+                                                         jlong mat_addr_input,
+                                                         jlong mat_addr_result) {
+    // TODO: implement ConvertRGBtoGray()
+    Mat &matInput = *(Mat *)mat_addr_input;
+    Mat &matResult = *(Mat *)mat_addr_result;
+
+    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
+}
